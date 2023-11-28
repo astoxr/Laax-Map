f2a9b60ff5362359a985fe97a8b64d63c7757cfd
astoxr <lukaslehner16@gmail.com>
2023-11-28 13:20:55 +0100

    Upgraded

--- UI-Basic.py
+++ UI-Basic.py
@@ -2,16 +2,17 @@ import tkinter as tk
 from PIL import Image, ImageTk
 
 
+
 root = tk.Tk()
 root.title("Laax-Map")
 root.geometry("800x500")
 
 # Load the JPG image
-image_path = "C:\Users\lukas\Documents\GitHub\Laax-Map"  # Replace with the path to your JPG file
+image_path = "Flims-Laax_pistemap.jpg"
+
 image = Image.open(image_path)
 photo = ImageTk.PhotoImage(image)
 
-# Create a label to display the image
 label = tk.Label(root, image=photo)
 label.pack(padx=10, pady=10)
 
