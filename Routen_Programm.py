6ac4242d5d55c361750ea503b391d6f66ae85678
Sunnyboy77 <lennylantic7@gmail.com>
2023-12-12 08:25:38 +0100

    Update Routen_Programm.py

--- Routen_Programm.py
+++ Routen_Programm.py
@@ -155,4 +155,13 @@ karte_json=[
 karte = karte_json
 
 
-print(karte[0]["Name"])
+# So kannst du auf einzelne Dateien des JSON zugreiffen: print(karte[0]["Name"])
+
+def Du_zu_JSON(du):
+    du=input("Wo bist du?")
+    for i in len(karte):
+        if karte[i]["Name"]==du:
+            du = karte[i]
+            return(du)
+            break
+Du_zu_JSON
