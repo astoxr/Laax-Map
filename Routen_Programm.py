64fb8f4a1a565adc868f8fc86aea3b548a6cd3f8
Sunnyboy77 <lennylantic7@gmail.com>
2024-01-08 20:36:26 +0100

    Programm weiter gemacht (Vermutlich fertig, nur noch ausbauen auf weitere Wege + Json probleme)

--- Routen_Programm.py
+++ Routen_Programm.py
@@ -5,6 +5,10 @@ with open('Daten_für_Karte.json', 'r') as routen:
     karte = json.load(routen)
 # Lade die JSON-Daten
 
+alle_wege=[]
+
+
+
 
 # So kannst du auf einzelne Dateien des JSON zugreiffen: print(karte[0]["Name"])
 
@@ -39,36 +43,37 @@ def Position_zu_JSON(Position):
             return i
 
 #Mit diese Befehl wird die kürzeste Verbindung zwischen zwei Orten gesucht. Mehr erklärungen folgen
+
 def Weg_finden():
     #Hier werden die beiden Punkte, von denen man hin und her will definiert.
     Anfang = input("Wo startest du?")
     Anfang = Du_zu_JSON(Anfang)
     Ende = input("Wo willst du hin?")
     Ende = Du_zu_JSON(Ende)
-    richtiger_weg = range(999)
     #Hier started die Suche. Es beginnt damit, dass alle verschiedenen Anfänge von Wohin vom Anfang durchgegangen werden.
     for i in Anfang["Wohin"]:
         weg = [Anfang["Position"]]
         weg.append(i)
         if i == Ende["Position"]:
-            #Die Probleme sind: Die Liste wird immer Länger und löscht die Falschen Routen nicht. Der WEg ist noch nicht der richtige (ich vermute es bräuchte noch mehrere Einheiten).
-            if len(weg) < len(richtiger_weg):
-                richitger_weg= weg
+            alle_wege.append(weg)
+            break
         else:
             naechster_ort = Position_zu_JSON(i)
             for j in naechster_ort["Wohin"]:
                 weg.append(j)
                 if j == Ende["Position"]:
-                    if len(weg) < len(richtiger_weg):
-                        richtiger_weg=weg
+                    alle_wege.append(weg)
+                    break
                 else:
                     naechster_ort = Position_zu_JSON(j)
                     for k in naechster_ort["Wohin"]:
                         weg.append(k)
-                        if k == Ende ["Position"]:
-                            if len(weg)<len(richtiger_weg):
-                                richtiger_weg=weg
+                        if k == Ende["Position"]:
+                            alle_wege.append(weg)
+                            break
+                        else:
+                             break
 
-    return(richtiger_weg)
-print(Weg_finden())
-                    
+Weg_finden()
+print(alle_wege)
+            
---
+++ Test.py
@@ -0,0 +1,8 @@
+for i in range(100):
+    for k in range(20):
+        if k == 5:
+            print (5)
+            break
+        elif k == 7:
+            print(7)
+    
